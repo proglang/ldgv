77daab00bb69b32a3e0c38cd43c3bef19f7ef497
Janek Spaderna <janek@spaderna.eu>
2021-01-05 01:43:20 +0100

    Update to latest reflex-platform

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
     url = "https://github.com/${owner}/${repo}/tarball/${rev}";
   };
   reflex-platform = fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).reflex-platform;
-in (import reflex-platform { system = builtins.currentSystem; }).project ({ pkgs, ... }: {
+in (import reflex-platform {}).project ({ pkgs, ... }: {
   useWarp = true;
   withHoogle = false;
   packages = {
