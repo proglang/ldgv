4bcdbc92d550b969c181577c053c34bd7fea62f6
Janek Spaderna <janek@spaderna.eu>
2021-03-01 17:00:51 +0100

    [C] Functions to invoke the compiler

--- src/C/Compile.hs
+++ src/C/Compile.hs
@@ -1,13 +1,58 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wall #-}
 
-module C.Compile (listBackends, cflags) where
+module C.Compile
+  ( Env(..)
+  , defaultEnv
+  , compile
+  , link
+  , cflags
+  , listBackends
+  ) where
 
+import Control.Monad.IO.Class
+import Control.Monad.Reader
 import Data.Traversable
 import Paths_ldgv
 import System.FilePath
+import System.Process.Typed
+
+data Env = Env
+  { envCC :: String
+  , envLD :: String
+  , envCCFlags :: [String]
+  , envLDFlags :: [String]
+  }
+  deriving stock (Show)
+
+defaultEnv :: Env
+defaultEnv = Env
+  { envCC = "cc"
+  , envLD = "cc"
+  , envCCFlags = []
+  , envLDFlags = []
+  }
+
+compile :: (MonadReader Env m, MonadIO m) => FilePath -> FilePath -> m ()
+compile result src = do
+  cc <- asks envCC
+  defaultFlags <- liftIO cflags
+  customFlags <- asks envCCFlags
+  let args = "-o" : result : "-c" : src : defaultFlags ++ customFlags
+  runProcess_ $ proc cc args
+
+link :: (MonadReader Env m, MonadIO m) => FilePath -> [FilePath] -> m ()
+link result srcs = do
+  ld <- asks envLD
+  defaultFlags <- liftIO cflags
+  customFlagsA <- asks envCCFlags
+  customFlagsB <- asks envLDFlags
+  let args = "-o" : result : srcs ++ defaultFlags ++ customFlagsA ++ customFlagsB
+  runProcess_ $ proc ld args
 
 basePath :: FilePath
 basePath = "c-support/runtime"
