8c84b7ecc14d1fa7b507b490a0c0fb2dff36b019
Janek Spaderna <janek@spaderna.eu>
2021-04-17 19:11:17 +0200

    Unclutter the project root
    Nix support files now live in their own directory

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
     inherit sha256;
     url = "https://github.com/${owner}/${repo}/tarball/${rev}";
   };
-  reflex-platform = fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).reflex-platform;
+  reflex-platform = fetcher (builtins.fromJSON (builtins.readFile ./nix/versions.json)).reflex-platform;
 in (import reflex-platform {}).project ({ pkgs, ... }: {
   useWarp = true;
   withHoogle = false;
